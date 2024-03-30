8df9604098434ff69f37a42782ba344de688c67b
franklin <franklinchukwuemeka234@gmail.com>
2024-03-31 00:19:39 +0200

    output results commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,32 @@ import FormOutput from './components/FormOutput';
 const App = () => {
 
   const [values, setValues] = useState({
-    invoiceAmount: 10000,
-    advanceRate: 90,
-    interestRate: 3,
-    paymentTerm: 30,
-    commFee: 0.3,
+  invoiceAmount: 10000,
+  advanceRate: 90,
+  interestRate: 3,
+  paymentTerm: 30,
+  commFee: 0.3,
+  invoiceAmountPercent: "0",
+  financingCharge: "0.00"
   });
+
+  const labels = [
+    {
+      id: 1,
+      name: "invoiceAmountPercent",
+      type: "number",
+      label: "Invoice Amount Percent",
+      value: values.invoiceAmountPercent
+    },
+    {
+      id: 2,
+      name: "financingCharge",
+      type: "number",
+      label: "Financing Charge",
+      value: values.financingCharge,
+      step: "0.01"
+    }
+  ]
 
   const inputs = [
     {
@@ -67,21 +87,55 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+  
+    //Parse form input values to float
+    const invoiceAmount = parseFloat(values.invoiceAmount);
+    const advanceRate = parseFloat(values.advanceRate) / 100;
+    const interestRate = parseFloat(values.interestRate) / 100;
+    const paymentTerm = parseFloat(values.paymentTerm);
+    const commFee = parseFloat(values.commFee) / 100;
+  
+    //Perform calculation based on parsed values
+    //amount advanced
+    const amountAdvanced = invoiceAmount * advanceRate;
 
+    //daily interest cost
+    const dailyInterestCost = amountAdvanced * paymentTerm * (interestRate / 365);
+
+    //financing charge
+    const financingCharge = dailyInterestCost + (commFee * invoiceAmount);
+    const invoiceAmountPercent = ((financingCharge * 100) / invoiceAmount);
+  
+    // Update state with the calculated values
+    setValues({
+      ...values,
+      financingCharge: (financingCharge).toFixed(2),
+      invoiceAmountPercent: (invoiceAmountPercent).toFixed(2)
+    });
   };
-
+  
   const onChange = (e) => {
     setValues({...values, [e.target.name]: e.target.value});
   }
 
-  return <div className='App'>
+  return <div className ='App'>
     <form onSubmit = {handleSubmit}>
-      <h2>Factoring calculator</h2>
-      {inputs.map((input) => (
-      <FormInput key = {input.id} {...input} value = {values[input.name]} onChange = {onChange} />
-      ))}
-      <button className="infoBtn" id="calculate">Calculate</button>
-      </form>
+      <div className="formField">
+        <h2>Factoring calculator</h2>
+        {inputs.map((input) => (
+        <FormInput key = {input.id} {...input} value = {values[input.name]} onChange = {onChange} />
+        ))}
+        <button className="infoBtn" id="calculate">Calculate</button>
+      </div>
+
+      <div className="formResult">
+        <label>Invoice financing expenses</label>
+        {labels.map((value) => (
+        <FormOutput key = {value.id} {...value} value = {values[value.name]} onChange = {onChange} />
+        ))}
+        <button className="warningBtn">Apply</button>
+      </div>
+    </form>
   </div>
 }
 
--- src/components/FormOutput.jsx
+++ src/components/FormOutput.jsx
@@ -1,12 +1,12 @@
+import React, { useState } from "react";
 import "./form.css";
 
-const FormOutput = () => {
+const FormOutput = ({ value, id }) => {
 
   return (
     <div className="formInput">
-        
+        {id == 1 ? (<p> {value} <label> % </label> </p>) : (<p> {value} <label> EUR </label> </p>)}
     </div>
-    
   );
 };
 
