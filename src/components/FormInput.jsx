00341bbaae9c3e2900caea808043c3c2cc8a6ceb
franklin <franklinchukwuemeka234@gmail.com>
2024-03-31 00:11:29 +0200

    form field styles commit

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./form.css";
 
 const FormInput = ({ label, onChange, options, ...inputProps }) => {
   const [focused, setFocused] = useState(false);
